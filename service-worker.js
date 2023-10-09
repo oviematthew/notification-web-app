71a3a9a407cdbfd04c6a1638e373459b961f1f1a
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2023-10-09 11:21:18 -0400

    cache added

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,43 @@
+const cacheName = 'my-music-app-cache-v1';
 
-// On install of service worker
 self.addEventListener('install', function (event) {
-    console.log('Servce Worker Installed', event);
+  console.log('Service Worker Installed', event);
 
-    self.skipWaiting();
-    });
-
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(function (cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/js/script.js',
+          '/css/style.css',
+          '/assets/icons/trash-can-solid.svg',
+        ]);
+      })
+  );
+});
 
 self.addEventListener('activate', function (event) {
-        console.log('Servce Worker Activated', event);
-        });
+  console.log('Service Worker Activated', event);
+
+  event.waitUntil(
+    caches.keys().then( (cacheNames) => {
+      return Promise.all(
+        cacheNames.map(function (name) {
+          if (name !== cacheName) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
 
 self.addEventListener('fetch', function (event) {
-        return event;
-        });
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        return response || fetch(event.request);
+      })
+  );
+});
